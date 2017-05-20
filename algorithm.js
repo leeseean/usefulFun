032bb736b3c7f73a482b0c29978dc180208d989e
leeseean chiu <leeseean@qq.com>
2017-05-21 00:30:36 +0800

    add

---
+++ algorithm.js
@@ -0,0 +1,726 @@
+/*常用算法函数*/
+/*去重*/
+function delRepeat(arr) {
+
+  var newArray = new Array();
+
+  var len = arr.length;
+
+  for (var i = 0; i < len; i++) {
+
+    for (var j = i + 1; j < len; j++)
+
+    {
+
+      if (arr[i] == arr[j])
+
+      {
+
+        ++i;
+
+      }
+
+    }
+
+    newArray.push(arr[i]);
+
+  }
+
+  return newArray;
+
+}
+/*二分法,又称为折半查找算法，但是有缺陷就是要求数字是预先排序好的*/
+function binary(items, value) {
+
+  var startIndex = 0,
+
+    stopIndex = items.length - 1,
+
+    midlleIndex = (startIndex + stopIndex) >>> 1;
+
+  while (items[middleIndex] != value && startIndex < stopIndex) {
+
+    if (items[middleIndex] > value) {
+
+      stopIndex = middleIndex - 1;
+
+    } else {
+
+      startIndex = middleI3ndex + 1;
+
+    }
+
+    middleIndex = (startIndex + stopIndex) >>> 1;
+
+  }
+
+  return items[middleIndex] != value ? false : true;
+
+}
+/*十六进制颜色值的随机生成*/
+
+function randomColor() {
+
+  var arrHex = ["0", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d"],
+
+    strHex = "#",
+
+    index;
+
+  for (var i = 0; i < 6; i++) {
+
+    index = Math.round(Math.random() * 15);
+
+    strHex += arrHex[index];
+
+  }
+
+  return strHex;
+
+}
+
+/*一个求字符串长度的方法,汉字算两个字符*/
+
+function GetBytes(str) {
+
+  var len = str.length,
+
+    bytes = len;
+
+  for (var i = 0; i < len; i++) {
+
+    if (str.CharCodeAt > 255) {
+
+      bytes++;
+
+    }
+
+  }
+
+  return bytes;
+
+}
+
+/*插入排序，所谓的插入排序，就是将序列中的第一个元素看成一个有序的子序列，然后不段向后比较交换比较交换。*/
+
+function insertSort(arr) {
+
+  var key;
+
+  for (var j = 1; j < arr.length; j++) {
+    //排好序的
+    var i = j - 1;
+
+    key = arr[j];
+
+    while (i >= 0 && arr[i] > key) {
+
+      arr[i + 1] = arr[i];
+
+      i--;
+
+    }
+
+    arr[i + 1] = key;
+
+  }
+
+  return arr;
+
+}
+
+
+
+
+
+
+
+
+
+/*希尔排序，希尔排序，也称递减增量排序算法具体描述，其实说到底也是插入排序的变种*/
+
+function shellSort(array) {
+
+  var stepArr = [1750, 701, 301, 132, 57, 23, 10, 4, 1]; // reverse()在维基上看到这个最优的步长较小数组
+
+  var i = 0;
+
+  var stepArrLength = stepArr.length;
+
+  var len = array.length;
+
+  var len2 = parseInt(len / 2);
+
+  for (; i < stepArrLength; i++) {
+
+    if (stepArr[i] > len2) {
+
+      continue;
+
+    }
+
+    stepSort(stepArr[i]);
+
+  }
+
+  // 排序一个步长
+
+  function stepSort(step) {
+
+    //console.log(step) 使用的步长统计
+
+    var i = 0,
+      j = 0,
+      f, tem, key;
+
+    var stepLen = len % step > 0 ? parseInt(len / step) + 1 : len / step;
+
+    for (; i < step; i++) { // 依次循环列
+
+      for (j = 1; /*j < stepLen && */ step * j + i < len; j++) { //依次循环每列的每行
+
+        tem = f = step * j + i;
+
+        key = array[f];
+
+
+
+        while ((tem -= step) >= 0) { // 依次向上查找
+
+          if (array[tem] > key) {
+
+            array[tem + step] = array[tem];
+
+          } else {
+
+            break;
+
+          }
+
+        }
+
+        array[tem + step] = key;
+
+      }
+
+    }
+
+  }
+
+  return array;
+
+}
+
+
+
+/*快速排序，其实说到底快速排序算法就系对冒泡排序的一种改进，采用的就是算法理论中的分治递归的思想，说得明白点，它的做法就是：通过一趟排序将待排序的纪录分割成两部分，其中一部分的纪录值比另外一部分的纪录值要小，就可以继续分别对这两部分纪录进行排序；不段的递归实施上面两个操作，从而实现纪录值的排序。
+
+这么说可能不是很清晰，直接上代码：*/
+
+
+function sort(arr) {
+
+  return quickSort(arr, 0, arr.length - 1);
+
+  function quickSort(arr, l, r) {
+
+    if (l < r) {
+
+      var mid = arr[parseInt((l + r) / 2)],
+        i = l - 1,
+        j = r + 1;
+
+      while (true) {
+
+        //大的放到右边，小的放到左边, i与j均为游标
+
+        while (arr[++i] < mid);
+
+        while (arr[--j] > mid);
+
+        if (i >= j) break; //判断条件
+
+        var temp = arr[i];
+
+        arr[i] = arr[j];
+
+        arr[j] = temp;
+
+      }
+
+      quickSort(arr, l, i - 1);
+
+      quickSort(arr, j + 1, r);
+
+    }
+
+    return arr;
+
+  }
+
+}
+
+
+/*冒泡法*/
+
+function bullSort(array) {
+
+  var temp;
+
+  for (var i = 0; i < array.length; i++) {
+
+    for (var j = array.length - 1; j > i; j--) {
+
+      if (array[j] < array[j - 1]) {
+
+        temp = array[j];
+
+        array[j] = array[j - 1];
+
+        array[j - 1] = temp;
+
+      }
+
+    }
+
+  }
+
+  return array;
+
+}
+
+
+
+/*js递归实现方案*/
+
+
+function factorial(num) {
+
+  if (num <= 1) {
+
+    return 1;
+
+  } else {
+
+    return num * factorial(num - 1);
+
+  }
+
+}
+
+
+/*选择法排序*/
+
+
+function selectionSort(data) {
+
+  var i, j, min, temp, count = data.length;
+
+  for (i = 0; i < count - 1; i++) {
+
+    /* find the minimum */
+
+    min = i;
+
+    for (j = i + 1; j < count; j++)
+
+    {
+      if (data[j] < data[min])
+
+      {
+        min = j;
+      }
+
+    }
+
+    /* swap data[i] and data[min] */
+
+    temp = data[i];
+
+    data[i] = data[min];
+
+    data[min] = temp;
+
+  }
+
+  return data;
+
+}
+
+
+
+/**
+
+* 堆排序
+
+* @param items 数组
+
+* @return 排序后的数组
+
+*/
+
+function heapSort(items) {
+
+  items = array2heap(items); //将数组转化为堆
+
+  for (var i = items.length - 1; i >= 0; i--)
+
+  {
+
+    items = swap(items, 0, i); //将根和位置i的数据交换(用于将最大值放在最后面)
+
+    items = moveDown(items, 0, i - 1); //数据交换后恢复堆的属性
+
+  }
+
+  return items;
+
+}
+
+/**
+
+* 将数组转换为堆
+
+* @param items 数组
+
+* @return 堆
+
+*/
+
+function array2heap(items) {
+
+  for (var i = Math.ceil(items.length / 2) - 1; i >= 0; i--)
+
+  {
+
+    items = moveDown(items, i, items.length - 1); //转换为堆属性
+
+  }
+
+  return items;
+
+}
+
+/**
+
+* 转换为堆
+
+* @param items 数组
+
+* @param first 第一个元素
+
+* @param last 最后一个元素
+
+* @return 堆
+
+*/
+
+function moveDown(items, first, last) {
+
+  var largest = 2 * first + 1;
+
+  while (largest <= last)
+
+  {
+
+    if (largest < last && items[largest] < items[largest + 1])
+
+    {
+
+      largest++;
+
+    }
+
+    if (items[first] < items[largest])
+
+    {
+
+      items = swap(items, first, largest); // 交换数据
+
+      first = largest; //往下移
+
+      largest = 2 * first + 1;
+
+    } else
+
+    {
+
+      largest = last + 1; //跳出循环
+
+    }
+
+  }
+
+  return items;
+
+}
+
+/**
+
+* 交换数据
+
+* @param items 数组
+
+* @param index1 索引1
+
+* @param index2 索引2
+
+* @return 数据交换后的数组
+
+*/
+
+function swap(items, index1, index2) {
+
+  var tmp = items[index1];
+
+  items[index1] = items[index2];
+
+  items[index2] = tmp;
+
+  return items;
+
+}
+
+
+
+
+
+
+
+
+
+/*所谓归并就是将两个或者两个以上的有序表合成一个新的有序表。
+
+递归形式的算法在形式上较为简洁但实用性较差，与快速排序和堆排序相比，归并排序的最大特点是，它是一种稳定的排序方法。
+
+js实现归并：*/
+
+
+function MemeryArray(Arr, n, Brr, m) {
+  var i, j, k;
+
+  var Crr = new Array();
+
+  i = j = k = 0;
+
+  while (i < n && j < m)
+
+  {
+
+    if (Arr[i] < Brr[j])
+
+      Crr[k++] = Arr[i++];
+
+    else
+
+      Crr[k++] = Brr[j++];
+
+  }
+
+  while (i < n)
+
+    Crr[k++] = Arr[i++];
+
+  while (j < m)
+
+    Crr[k++] = Brr[j++];
+
+  return Crr;
+
+}
+
+
+
+
+
+//将有二个有序数列a[first...mid]和a[mid...last]合并。
+
+function mergearray(Arr, first, mid, last, tempArr) {
+
+  var i = first,
+    j = mid + 1;
+
+  var m = mid,
+    n = last;
+
+  var k = 0;
+
+  while (i <= m && j <= n)
+
+  {
+
+    if (Arr[i] < Arr[j])
+
+      tempArr[k++] = Arr[i++];
+
+    else
+
+      tempArr[k++] = Arr[j++];
+
+  }
+
+  while (i <= m)
+
+    tempArr[k++] = Arr[i++];
+
+  while (j <= n)
+
+    tempArr[k++] = Arr[j++];
+
+  for (i = 0; i < k; i++)
+
+    Arr[first + i] = tempArr[i];
+
+}
+
+function mergesort(Arr, first, last) {
+
+  var tempArr = new Array();
+
+  if (first < last)
+
+  {
+
+    var mid = (first + last) >>> 1;
+
+    mergesort(Arr, first, mid, tempArr); //左边有序
+
+    mergesort(Arr, mid + 1, last, tempArr); //右边有序
+
+    mergearray(Arr, first, mid, last, tempArr); //再将二个有序数列合并
+
+  }
+
+  return Arr;
+
+}
+
+
+
+
+
+
+//求两个字符串的相似度,返回差别字符数,Levenshtein Distance算法实现
+
+function Levenshtein_Distance(s, t) {
+
+  　
+  var n = s.length; // length of s
+
+  　
+  var m = t.length; // length of t
+
+  　
+  var d = []; // matrix
+
+  　
+  var i; // iterates through s
+
+  　
+  var j; // iterates through t
+
+  　
+  var s_i; // ith character of s
+
+  　
+  var t_j; // jth character of t
+
+  　
+  var cost; // cost
+
+  　 // Step 1
+
+  　
+  if (n == 0) return m;
+
+  　
+  if (m == 0) return n;
+
+  　 // Step 2
+
+  　
+  for (i = 0; i <= n; i++) {
+
+    　　
+    d[i] = [];
+
+    　　
+    d[i][0] = i;
+
+    　
+  }
+
+  　
+  for (j = 0; j <= m; j++) {
+
+    　　
+    d[0][j] = j;
+
+    　
+  }
+
+  　 // Step 3
+
+  　
+  for (i = 1; i <= n; i++) {
+
+    　　
+    s_i = s.charAt(i - 1);
+
+    　　 // Step 4
+
+    　　
+    for (j = 1; j <= m; j++) {
+
+      　　　
+      t_j = t.charAt(j - 1);
+
+      　　　 // Step 5
+
+      　　　
+      if (s_i == t_j) {
+
+        　　　　
+        cost = 0;
+
+        　　　
+      } else {
+
+        　　　　
+        cost = 1;
+
+        　　　
+      }
+
+      　　　 // Step 6
+
+      　　　
+      d[i][j] = Minimum(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost);
+
+      　　
+    }
+
+    　
+  }
+
+  　 // Step 7
+
+  　
+  return d[n][m];
+
+}
+
+//求两个字符串的相似度,返回相似度百分比
+
+function Levenshtein_Distance_Percent(s, t) {
+
+  　
+  var l = s.length > t.length ? s.length : t.length;
+
+  　
+  var d = Levenshtein_Distance(s, t);
+
+  　
+  return (1 - d / l).toFixed(4);
+
+}
---
+++ regular.js
@@ -0,0 +1,312 @@
+//定义两个全局变量
+var reg;
+var errorInfo;
+//每个验证都需要调用此方法
+function verifyByReg(value, reg, errorInfo) {
+    if (value != '' && reg != '') {
+        var regExp = new RegExp(reg);
+        if (!regExp.test(value)) {
+            alert(errorInfo);
+            throw errorInfo;
+        }
+    }
+}
+//输入的数字类型必须是int型,正负整数都可以
+function validate_integer(text, value) {
+    reg = /^[-\+]?\d+$/;
+    errorInfo = text + " " + value + " 只能是整数,请核实重新输入!";
+    verifyByReg(value, reg, errorInfo);
+}
+//输入的数字类型必须是double型,保留的小数位数只能是2位,可以为正负
+function validate_double(text, value) {
+    reg = /^[-\+]?([1-9](\d+)?|0)(\.\d{2})$/;
+    errorInfo = text + " " + value + " 只能保留2为小数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//输入的必须是全是数字类型型,不能出现其他字符
+function validate_number(text, value) {
+    reg = /^\d+$/;
+    errorInfo = text + " " + value + " 只能是数字,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证输入n位的数值类型
+function validate_number_n(text, value, n) {
+    reg = eval("/^\\d{" + n + "}$/");
+    errorInfo = text + " " + value + " 只能是" + n + "位数字,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证有两位小数的正实数
+function validate_decimal_two(text, value) {
+    reg = /^([1-9](\d+)?|0)(.[0-9]{2})?$/;
+    errorInfo = text + " " + value + " 只能是保留2位小数的正数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证非零的正整数
+function validate_number_positive(text, value) {
+    reg = /^\+?[1-9][0-9]*$/;
+    errorInfo = text + " " + value + " 只能是非0的正整数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证非零的负整数
+function validate_number_negative(text, value) {
+    reg = /^[-][1-9][0-9]*$/;
+    errorInfo = text + " " + value + " 只能是非0的负整数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证非负整数,可以为0
+function validate_positive_haszero(text, value) {
+    reg = /^[1-9](\d+)?|0$/;
+    errorInfo = text + " " + value + " 只能是是0或正整数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证非正整数,可以为0
+function validate_negative_haszero(text, value) {
+    reg = /^\-[1-9](\d+)?|0$/;
+    errorInfo = text + " " + value + " 只能是是0或负整数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证非负浮点数（正浮点数 + 0）
+function validate_float_notnegative(text, value) {
+    reg = /^([1-9](\d+)?|0)(\.\d+)|0$/;
+    errorInfo = text + " " + value + " 只能是是0或正浮点数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证正浮点数
+function validate_float_positive(text, value) {
+    reg = /^([1-9](\d+)?|0)(\.\d+)$/;
+    errorInfo = text + " " + value + " 只能是正浮点数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//非正浮点数（负浮点数 + 0）
+function validate_float_notpositive(text, value) {
+    reg = /^\-([1-9](\d+)?|0)(\.\d+)|0$/;
+    errorInfo = text + " " + value + " 只能是是0或者负浮点数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证负浮点数
+function validate_float_negative(text, value) {
+    reg = /^\-([1-9](\d+)?|0)(\.\d+)$/;
+    errorInfo = text + " " + value + " 只能是负浮点数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证正浮点数多少位小数
+function validate_float_posbit(text, value, n) {
+    reg = eval("/^(\\d+)(\\.\\d{" + n + "})$/");
+    errorInfo = text + " " + value + " 只能是" + n + "位正浮点数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证负浮点数多少位小数
+function validate_float_negbit(text, value, n) {
+    reg = eval("/^(-?\\d+)(\\.\\d{" + n + "})$/");
+    errorInfo = text + " " + value + " 只能是" + n + "位负浮点数,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//校验文本是否为空
+function checknull(text, value) {
+    if (value == "" || typeof (value) == 'undefined') {
+        errorInfo = text + " " + value + " 不能为空,请重新输入!";
+        returnInfo(errorInfo);
+    }
+}
+//验证由26个英文字母组成的字符串
+function validate_string_letter(text, value) {
+    reg = /^[A-Za-z]+$/;
+    errorInfo = text + " " + value + " 只能是26位不区分大小写的英文字母组成,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证由26个英文字母的大写组成的字符串
+function validate_string_bigletter(text, value) {
+    reg = /^[A-Z]+$/;
+    errorInfo = text + " " + value + " 只能是26位大写的英文字母组成,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证由26个英文字母的小写组成的字符串
+function validate_string_smallletter(text, value) {
+    reg = /^[a-z]+$/;
+    errorInfo = text + " " + value + " 只能是26位小写的英文字母组成,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证由数字和26个英文字母组成的字符串
+function validate_string_number(text, value) {
+    reg = /^[A-Za-z0-9]+$/;
+    errorInfo = text + " " + value + " 只能是数字和26个英文字母组成,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证由数字、26个英文字母或者下划线组成的字符串
+function validate_string_numberandunderline(text, value) {
+    reg = /^\w+$/;
+    errorInfo = text + " " + value + " 只能是数字、26个英文字母或者下划线组成,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证邮箱
+function validate_email(value) {
+    reg = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/;
+    errorInfo = '邮箱 ' + value + " 无效,请重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证时如果选择默认调用此方法 金额类型,可以是正整数或保留4位有效数字的正整数
+/**有问题
+function validate_money_default(text,value){
+var reg=/^\d+(\.\d{4})?$/;
+var errorInfo=text+" "+value+" 只能是正整数或保留4位有效数字,请核实重新输入";
+verifyByReg(value,reg,errorInfo);
+}
+**/
+//验证输入的只能是中文
+function validate_chinese(text, value) {
+    reg = /^[\u4e00-\u9fa5]+$/;
+    errorInfo = text + " " + value + " 只能是中文字符,请重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证输入的只能是n位的中文
+function validate_bitchinese(text, value, bit) {
+    reg = eval("/^[\\u4e00-\\u9fa5]{" + bit + "}$/");
+    alert(reg);
+    errorInfo = text + " " + value + " 只能是" + bit + "位中文字符,请重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//跟最大的值比较,此方法是看数字是否超出系统的上限
+function thanMax(text, number, max) {
+    if (number > max) {
+        errorInfo = text + " " + number + " 不能大于" + max + ",请重新输入";
+        returnInfo(errorInfo);
+    }
+}
+//跟最小的值比较,此方法是看数字是否低于系统的下限
+function thinMin(text, number, min) {
+    if (number < min) {
+        errorInfo = text + " " + number + " 不能小于" + min + ",请重新输入";
+        returnInfo(errorInfo);
+    }
+}
+//输入文本的字符串是否在系统的指定长度内
+function isLimit(text, value, length) {
+    var arr = value.split("");
+    if (arr.length > length) {
+        errorInfo = text + " " + value + " 字符过长,请输入最多" + length + "位字符";
+        returnInfo(errorInfo);
+    }
+}
+//邮政编码的验证
+function validate_zipcode(value) {
+    reg = /[1-9]\d{5}(?!\d)/;
+    errorInfo = '邮政编码 ' + value + " 有误,请核实重新输入";
+    verifyByReg(value, reg, errorInfo);
+}
+//对数值类型的数据进行简单的验证和转换,因为在设置的时候没有传入这个参数的话就会出现js代码错误，所以应该对数值型的数据进行转换
+//非空字符的判定
+function getZeroByNull(text, value) {
+    errorInfo = text + " " + value + " 不能为空,请核实重新输入";
+    if (value == null || value == '' || value == 'undefined') {
+        returnInfo(errorInfo);
+    }
+}
+//身份证验证,分为了两种,15位和18位
+function validate_IdCard(value) {
+    var size = value.split("");
+    if (size.length == 15) {
+        reg = /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/; //15位身份证验证
+        errorInfo = '位身份证号码 ' + value + " 有误,请核实重新输入";
+    } else if (size.length == 18) {
+        reg = /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X|x)$/; //18位身份证验证
+        errorInfo = '位身份证号码 ' + value + " 有误,请核实重新输入";
+    } else {
+        errorInfo = "请输入合法的身份证号码!";
+        returnInfo(errorInfo);
+    }
+    verifyByReg(value, reg, errorInfo);
+}
+//不进过正则表达式验证,只需要返回信息,适合内部调用此方法
+function returnInfo(message) {
+    alert(message);
+    throw message;
+}
+//许可证号 生产企业的格式：QS 1234 2346 3456 其中空格可有可无
+///经营企业许可证的格式为: 湘010249 但是中国的省会简称可能会有三个字的，所以中文可以定义为1-3个
+function validate_license(value, type) {
+    if (type == 1) {
+        reg = /^[a-zA-Z]{2}[ ]?(\d{4}[ ]?\d{4}[ ]?\d{4})$/; //生产企业许可证号
+        errorInfo = "生产许可证号 " + value + " 有误,请核实重新输入!";
+    } else if (type == 2) {
+        reg = /^[\u4e00-\u9fa5]{1,3}\d{6}$/;
+        errorInfo = "经营许可证号 " + value + " 有误,请核实重新输入!";
+    }
+    verifyByReg(value, reg, errorInfo);
+}
+//传真 可以匹配的字符串如：+123 -999 999 ； +123-999 999 ；123 999 999 ；+123 999999等
+function validate_fax(value) {
+    reg = /^([+]{0,1})?\d{1,3}[ ]?([-]?((\d)|[ ]){1,12})+$/;
+    errorInfo = "传真 " + value + " 有误,请重新输入!";
+    verifyByReg(value, reg, errorInfo);
+}
+//电话号码可以输入：电话或者手机 其中手机号码可以是(+86)、(86)+号码，也可以是号码 电话的号码可以是区号+号码 区号-号码 号码
+function validate_tel(value) {
+    reg = /(^(0\d{2,3}([-|+])?)?\d{7}$)|(^(\d{3,4}([-|+])?)?\d{7,8}$)|(^([+]?(86)?[+]?)1[3-9]\d{9}$)/;
+    errorInfo = '电话号码 ' + value + " 有误,请核实重新输入!";
+    verifyByReg(value, reg, errorInfo);
+}
+//日：大于0小于等于31的正整数
+function validate_day(value) {
+    reg = /(^[0]?[1-9]$)|(^[1-2]\d$)|(^[3][0-1]$)/;
+    errorInfo = '日期 ' + value + " 有误,请核实重新输入!";
+    verifyByReg(value, reg, errorInfo);
+}
+//月：合法月份
+function validate_month(value) {
+    reg = /(^[0]?[1-9]$)|(^[1][0-2]$)/;
+    errorInfo = '月份 ' + value + " 有误,请核实重新输入!";
+    verifyByReg(value, reg, errorInfo);
+}
+//年：合法年份 在这里合法的年份为公元1000年至公元2999年
+function validate_year(value) {
+    reg = /^[1-2]\d{3}$/;
+    errorInfo = '年份 ' + value + " 有误,请核实重新输入!";
+    verifyByReg(value, reg, errorInfo);
+}
+//百分比：合法百分比（0-100之前的数字）
+function validate_percentage(text, value) {
+    reg = /(^[1-9](\d)?(\.\d+)?$)|(^0(\.\d+)?$)|(^100$)/;
+    errorInfo = text + " " + value + " 有误,请输入0到100的数字!";
+    verifyByReg(value, reg, errorInfo);
+}
+//系数：小于等于1的正数 在这里系数的小数点后面也是2位
+function validate_modulus(text, value) {
+    reg = /^[0](\.\d{2})$|^[1](\.[0]{2})?$/;
+    errorInfo = text + " " + value + " 有误,请输入小于0到1的数字!";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证非负的正数，包含了正整数和正浮点数
+function validate_posnumhaszero(text, value) {
+    checknull(text, value);
+    reg = /^[1-9](\d+)?(\.\d+)?$|^0(\.\d+)?$/;
+    errorInfo = text + " " + value + " 有误，请输入大于或等于0 的数字!";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证非正的负数，包含了负整数和负浮点数
+function validate_negnumhaszero(text, value) {
+    checknull(text, value);
+    reg = /^[-][1-9](\d+)?(\.\d+)?$|^[-]0(\.\d+)?$/;
+    errorInfo = text + " " + value + " 有误，请输入小于或等于0 的数字!";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证正数，包含了正整数和正浮点数
+function validate_posnum(text, value) {
+    checknull(text, value);
+    reg = /^[1-9](\d+)?(\.\d+)?$|^0(\.\d+)$/;
+    errorInfo = text + " " + value + " 有误，请输入大于0 的数字!";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证负数，包含了负整数和负浮点数
+function validate_negnum(text, value) {
+    checknull(text, value);
+    reg = /^[-][1-9](\d+)?(\.\d+)?$|^[-]0(\.\d+)$/;
+    errorInfo = text + " " + value + " 有误，请输入小于0 的数字!";
+    verifyByReg(value, reg, errorInfo);
+}
+//验证数,包括所有的数值,正数、负数、正浮点数、负浮点数
+function validate_allnum(text, value) {
+    checknull(text, value);
+    reg = /^[-|+]?[1-9](\d+)?(\.\d+)?$|^[-|+]?0(\.\d+)?$/;
+    errorInfo = text + " " + value + " 有误，请输入数字!";
+    verifyByReg(value, reg, errorInfo);
+}
