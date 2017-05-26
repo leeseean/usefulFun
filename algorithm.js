cf83c9ea808c2b68bc7b2f1fa047edf1ca03c102
leeseean <leeseean@qq.com>
2017-05-26 14:56:09 +0800

    加计算排列组合具体组合的方式[1,2,3]->[1,2],[1,3],[2,3]

--- algorithm.js
+++ algorithm.js
@@ -737,3 +737,54 @@ function difference(a, b) {
     return b.indexOf(item) === -1;
   });
 }
+//求数组排列的所有排列方式[1,2,3]->[1,2],[1,3],[2,3],[2,1],[3,1],[3,2]
+function queue(arr, size) {
+  if (size > arr.length) {
+    return;
+  }
+  var allResult = [];
+
+  (function (arr, size, result) {
+    if (result.length == size) {
+      allResult.push(result);
+    } else {
+      for (var i = 0, len = arr.length; i < len; i++) {
+        var newArr = [].concat(arr),
+          curItem = newArr.splice(i, 1);
+        arguments.callee(newArr, size, [].concat(result, curItem));
+      }
+    }
+  })(arr, size, []);
+
+  return allResult;
+}
+
+//求数组组合的所有组合方式[1,2,3]->[1,2],[1,3],[2,3]
+function choose(arr, size) {
+  var allResult = [];
+
+  (function (arr, size, result) {
+    var arrLen = arr.length;
+    if (size > arrLen) {
+      return;
+    }
+    if (size == arrLen) {
+      allResult.push([].concat(result, arr))
+    } else {
+      for (var i = 0; i < arrLen; i++) {
+        var newResult = [].concat(result);
+        newResult.push(arr[i]);
+
+        if (size == 1) {
+          allResult.push(newResult);
+        } else {
+          var newArr = [].concat(arr);
+          newArr.splice(0, i + 　1);
+          arguments.callee(newArr, size - 1, newResult);
+        }
+      }
+    }
+  })(arr, size, []);
+
+  return allResult;
+}
