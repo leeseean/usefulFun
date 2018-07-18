98911c48babde7952b33079c025ae0a7d77d984b
Leeseean Chiu <leeseeanchiu@gmail.com>
2018-07-18 23:37:04 +0800

    deepClone
    deepClone

--- functional.js
+++ functional.js
@@ -182,3 +182,23 @@ export const trim = function (str, flag) {
 export function range(num, step) {
     return [...Array(5).keys()].filter(v => v % step == 0);
 }
+/**
+ * deeply copy object or arrays with nested attributes
+ * @param  {any} obj
+ * @return {any}     a depply copied replica of arguement passed
+ */
+
+export const deepClone = (obj) => {
+  if (!obj || typeof obj !== 'object') {
+    return obj;
+  }
+  let newObj = {};
+  if (Array.isArray(obj)) {
+    newObj = obj.map(item => deepClone(item));
+  } else {
+    Object.keys(obj).forEach((key) => {
+      return newObj[key] = deepClone(obj[key]);
+    })
+  }
+  return newObj;
+}
